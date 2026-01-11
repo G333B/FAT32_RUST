a7c305547aa06526374969b3c798f81a85323968
G333B <clem971gwada@gmail.com>
2026-01-12 00:50:23 +0100

    lib.rs file

---
+++ fat32/src/lib.rs
@@ -0,0 +1,25 @@
+//! systemFAT32 LANGUEDOC CLEMENT 4SI5
+
+#![no_std]
+
+extern crate alloc;
+
+pub mod error;
+pub mod boot_sector;
+pub mod directory;
+pub mod fat_table;
+pub mod filesystem;
+
+// Re-export des types principaux
+pub use error::{Fat32Error, Result};
+pub use boot_sector::BootSector;
+pub use directory::{DirectoryEntry, FileAttributes};
+pub use fat_table::FatTable;
+pub use filesystem::Fat32FileSystem;
+
+/// read/write bock device abstraction
+pub trait BlockDevice {
+    fn read_sector(&mut self, sector: u32, buffer: &mut [u8]) -> Result<()>;
+    fn write_sector(&mut self, sector: u32, buffer: &[u8]) -> Result<()>;
+    fn sector_size(&self) -> usize;
+}
